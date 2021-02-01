c3dfb019e9eaf52dc9dbbfab793e0ab90fb43f44
Azraf Chowdhury <achowdhur@ltu.edu>
2021-02-01 07:09:06 +0000

    Automatically backed up by Learn

--- src/Comment.js
+++ src/Comment.js
@@ -1,2 +1,12 @@
 //your code here
 import React, { Component } from 'react'
+
+class Comment extends React.component {
+  render() {
+    return (
+      <div>
+        {this.props.commentText}
+      </div>
+    )
+  }
+}
